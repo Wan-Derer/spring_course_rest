7f038e05bca690226057c2a0da6f7624514ca7eb
Wan-Derer <frd-ru@yandex.ru>
2021-11-28 02:15:42 +0300

    Lesson 80

---
+++ src/main/java/com/zaurtregulov/spring/rest/configuration/MyConfig.java
@@ -0,0 +1,59 @@
+package com.zaurtregulov.spring.rest.configuration;
+
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@ComponentScan(basePackages = "com.zaurtregulov.spring.rest")
+@EnableWebMvc
+@EnableTransactionManagement
+public class MyConfig {
+
+  @Bean
+  public DataSource dataSource() {
+//    ComboPooledDataSource dataSource = new ComboPooledDataSource();
+    HikariDataSource dataSource = new HikariDataSource();
+    dataSource.setDriverClassName("org.firebirdsql.jdbc.FBDriver");
+    dataSource.setJdbcUrl("jdbc:firebirdsql://localhost:3050/W:/Work_2021/Spring/Spring for beginners (Zaur Tregulov)/spring_course_rest/db/test3.fdb?useSSL=false&amp;serverTimezone=UTC&amp;charSet=utf8");
+    dataSource.setUsername("user");
+    dataSource.setPassword("user");
+
+    return dataSource;
+  }
+
+  @Bean
+  public LocalSessionFactoryBean sessionFactory(DataSource dataSource){
+    LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+    sessionFactory.setDataSource(dataSource);
+    sessionFactory.setPackagesToScan("com.zaurtregulov.spring.rest.entity");
+
+    Properties hibernateProps = new Properties();
+    hibernateProps.setProperty("hibernate.dialect", "org.hibernate.dialect.FirebirdDialect");
+    hibernateProps.setProperty("hibernate.show_sql", "true");
+
+    sessionFactory.setHibernateProperties(hibernateProps);
+
+    return sessionFactory;
+  }
+
+  @Bean
+  public HibernateTransactionManager transactionManager(LocalSessionFactoryBean sessionFactory){
+
+    HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+    transactionManager.setSessionFactory(sessionFactory.getObject());
+
+    return transactionManager;
+  }
+
+
+}
